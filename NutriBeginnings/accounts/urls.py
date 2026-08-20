62694fe6e861f44219f8dd70990d78a25f05fafb
ShreyaTaral <shreyataral9@gmail.com>
2026-08-20 10:59:12 +0530

    Add files via upload

---
+++ NutriBeginnings/accounts/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'
+urlpatterns = [
+
+    # Home
+    path(
+        '',
+        views.home,
+        name='home'
+    ),
+
+    # Login
+    path(
+        'login/',
+        views.login_view,
+        name='login'
+    ),
+
+    # Register
+    path(
+        'register/',
+        views.register_view,
+        name='register'
+    ),
+
+    # Logout
+    path(
+        'logout/',
+        views.logout_view,
+        name='logout'
+    ),
+
+]
